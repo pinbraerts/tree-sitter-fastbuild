078a33112df5111ab36aed13d8b201402b845e48
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-03-03 22:29:08 +0300

    update bindings to fix lsp build

--- binding.gyp
+++ binding.gyp
@@ -1,7 +1,7 @@
 {
   "targets": [
     {
-      "target_name": "tree_sitter_FASTBuild_binding",
+      "target_name": "tree_sitter_fastbuild_binding",
       "include_dirs": [
         "<!(node -e \"require('nan')\")",
         "src"
--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -4,7 +4,7 @@
 
 using namespace v8;
 
-extern "C" TSLanguage * tree_sitter_FASTBuild();
+extern "C" TSLanguage * tree_sitter_fastbuild();
 
 namespace {
 
@@ -17,12 +17,12 @@ void Init(Local<Object> exports, Local<Object> module) {
 
   Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_FASTBuild());
+  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_fastbuild());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("FASTBuild").ToLocalChecked());
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("fastbuild").ToLocalChecked());
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
-NODE_MODULE(tree_sitter_FASTBuild_binding, Init)
+NODE_MODULE(tree_sitter_fastbuild_binding, Init)
 
 }  // namespace
