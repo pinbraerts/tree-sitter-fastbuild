90d14494980c6ee6977cb1edec0a0af9011fe6fc
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-07-03 22:12:05 +0300

    prettier . --write

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 [FASTBuild](https://github.com/fastbuild/fastbuild) grammar for [tree-sitter](https://github.com/tree-sitter/tree-sitter)
 
 ## Usage
+
 It's not published on grammar lists yet
 
 ### [NeoVim](https://github.com/neovim/neovim/) (using [nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter))
+
 ```lua
 local parsers = require 'nvim-treesitter.parsers'.get_parser_configs()
 vim.filetype.add {
@@ -30,24 +32,29 @@ vim.treesitter.language.register('fastbuild', 'fastbuild') -- not sure if you ne
 ```
 
 And then
+
 ```
 :TSInstall fastbuild
 ```
 
 ### Queries
+
 You need to copy queries to some nvim runtime path directory. I use `nvim-data/lazy/nvim-treesitter/queries/fastbuild`
 
 #### shell
+
 ```bash
 ln -s /path/to/cloned/tree-sitter-fastbuild/queries /some/nvim/rtp/path/queries/fastbuild
 ```
 
 #### powershell
+
 ```powershell
 New-Item -Type SymbolicLink -Path some\nvim\rtp\path\queries\fastbuild -Value path\to\cloned\tree-sitter-fastbuild\queries
 ```
 
 ## Features
+
 - [x] Highlighting
 - [x] Locals
 - [x] [Context](https://github.com/nvim-treesitter/nvim-treesitter-context)
@@ -56,7 +63,9 @@ New-Item -Type SymbolicLink -Path some\nvim\rtp\path\queries\fastbuild -Value pa
 - [ ] [Text Objects](https://github.com/nvim-treesitter/nvim-treesitter-textobjects)
 
 ## Links
+
 - [Syntax](https://fastbuild.org/docs/syntaxguide.html)
 
 ## Example
+
 ![examples/example.bff](pictures/example.png)
--- grammar.js
+++ grammar.js
@@ -1,226 +1,248 @@
 let precedence = {
-	add: 1,
-	sub: 1,
-	assign: 2,
-	or: 3,
-	and: 4,
-	compare: 5,
-	in: 6,
-	not_in: 6,
-	not: 7,
-	function: 8,
-}
+  add: 1,
+  sub: 1,
+  assign: 2,
+  or: 3,
+  and: 4,
+  compare: 5,
+  in: 6,
+  not_in: 6,
+  not: 7,
+  function: 8,
+};
 
 module.exports = grammar({
-	name: "fastbuild",
+  name: "fastbuild",
 
-	extras: $ => [
-		$.comment,
-		$._whitespace,
-		$._separator,
-		$._newline,
-		$._preprocessor,
-	],
+  extras: ($) => [
+    $.comment,
+    $._whitespace,
+    $._separator,
+    $._newline,
+    $._preprocessor,
+  ],
 
-	word: $ => $.identifier,
+  word: ($) => $.identifier,
 
-	supertypes: $ => [
-		$.literal,
-		$.expression,
-		$.statement,
-		$.number,
-		$.variable,
-		$.function,
-		$.preprocessor_expression,
-	],
+  supertypes: ($) => [
+    $.literal,
+    $.expression,
+    $.statement,
+    $.number,
+    $.variable,
+    $.function,
+    $.preprocessor_expression,
+  ],
 
-	inline: $ => [
-		$.filename_usage,
-	],
+  inline: ($) => [$.filename_usage],
 
-	externals: $ => [
-		$._preprocessor_end,
-	],
+  externals: ($) => [$._preprocessor_end],
 
-	rules: {
-		source_file: $ => repeat($.statement),
-		_separator:        _ => ',',
-		_whitespace:       _ => token(/[\s\u00A0\uFEFF\u3000]+/),                       
-		// http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
-		comment: _ => token(choice(
-			/;.*/,
-			/\/\/.*/,
-		)),
-		_newline:          _ => token(/\r?\n/),                         
-		decimal:           _ => token(/[\d_]+/), 
-		filename:          _ => token(/[\/\.\w\- ]+/),
-		identifier:        _ => token(/[a-zA-Z_][\w_]*/),               
-		single_quoted:     _ => token.immediate(prec(1, /[^^$%']+?/)),           
-		double_quoted:     _ => token.immediate(prec(1, /[^^$%"]+?/)),           
-		escape_sequence:   _ => token.immediate(/\^./),                 
-		string: $ => choice(
-			seq("'", repeat($._single_inner), "'"),
-			seq('"', repeat($._double_inner), '"'),
-		),
-		_single_inner: $ => choice(
-			$.single_quoted,
-			$.interpolation,
-			$.placeholder,
-			$.escape_sequence,
-		),
-		_double_inner: $ => choice(
-			$.double_quoted,
-			$.interpolation,
-			$.placeholder,
-			$.escape_sequence,
-		),
-		interpolation: $ => seq(token.immediate('$'), $.identifier, '$'),
-		placeholder:   $ => seq(token.immediate('%'),
-			choice(field('number', $.decimal), '@', '*'),
-		),
+  rules: {
+    source_file: ($) => repeat($.statement),
+    _separator: (_) => ",",
+    _whitespace: (_) => token(/[\s\u00A0\uFEFF\u3000]+/),
+    // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
+    comment: (_) => token(choice(/;.*/, /\/\/.*/)),
+    _newline: (_) => token(/\r?\n/),
+    decimal: (_) => token(/[\d_]+/),
+    filename: (_) => token(/[\/\.\w\- ]+/),
+    identifier: (_) => token(/[a-zA-Z_][\w_]*/),
+    single_quoted: (_) => token.immediate(prec(1, /[^^$%']+?/)),
+    double_quoted: (_) => token.immediate(prec(1, /[^^$%"]+?/)),
+    escape_sequence: (_) => token.immediate(/\^./),
+    string: ($) =>
+      choice(
+        seq("'", repeat($._single_inner), "'"),
+        seq('"', repeat($._double_inner), '"'),
+      ),
+    _single_inner: ($) =>
+      choice(
+        $.single_quoted,
+        $.interpolation,
+        $.placeholder,
+        $.escape_sequence,
+      ),
+    _double_inner: ($) =>
+      choice(
+        $.double_quoted,
+        $.interpolation,
+        $.placeholder,
+        $.escape_sequence,
+      ),
+    interpolation: ($) => seq(token.immediate("$"), $.identifier, "$"),
+    placeholder: ($) =>
+      seq(token.immediate("%"), choice(field("number", $.decimal), "@", "*")),
 
-		filename_usage: $ => choice(
-			seq('<', field("filename", $.filename), token.immediate('>')),
-			seq('"', field("filename", $.filename), token.immediate('"')),
-		),
+    filename_usage: ($) =>
+      choice(
+        seq("<", field("filename", $.filename), token.immediate(">")),
+        seq('"', field("filename", $.filename), token.immediate('"')),
+      ),
 
-		_preprocessor: $ => seq('#', choice(
-			$.preprocessor_define,
-			$.preprocessor_undef,
-			$.preprocessor_if,
-			$.preprocessor_import,
-			$.preprocessor_include,
-			$.preprocessor_once,
-			$.preprocessor_else,
-			$.preprocessor_endif,
-		), $._preprocessor_end),
-		preprocessor_define: $ => seq('define', field("variable", $.variable)),
-		preprocessor_undef: $ => seq('undef', field("variable", $.variable)),
-		preprocessor_import: $ => seq('import', field("variable", $.environment_variable)),
-		preprocessor_include: $ => seq('include', $.filename_usage),
-		preprocessor_if: $ => seq('if', field('condition', $.preprocessor_expression)),
-		preprocessor_else: _ => "else",
-		preprocessor_endif: _ => "endif",
-		preprocessor_once: _ => "once",
-		preprocessor_expression: $ => choice(
-			$.literal,
-			$.variable,
-			alias($.preprocessor_not, $.not),
-			alias($.preprocessor_and, $.and),
-			alias($.preprocessor_or, $.or),
-			$.exists,
-			$.file_exists,
-		),
-		preprocessor_or: $ => prec.left(precedence.or, seq(
-			field('left', $.preprocessor_expression),
-			'||',
-			field('right', $.preprocessor_expression),
-		)),
-		preprocessor_and: $ => prec.left(precedence.and, seq(
-			field('left', $.preprocessor_expression),
-			'&&',
-			field('right', $.preprocessor_expression),
-		)),
-		preprocessor_not: $ => prec.right(precedence.not, seq(
-			'!', $.preprocessor_expression,
-		)),
+    _preprocessor: ($) =>
+      seq(
+        "#",
+        choice(
+          $.preprocessor_define,
+          $.preprocessor_undef,
+          $.preprocessor_if,
+          $.preprocessor_import,
+          $.preprocessor_include,
+          $.preprocessor_once,
+          $.preprocessor_else,
+          $.preprocessor_endif,
+        ),
+        $._preprocessor_end,
+      ),
+    preprocessor_define: ($) => seq("define", field("variable", $.variable)),
+    preprocessor_undef: ($) => seq("undef", field("variable", $.variable)),
+    preprocessor_import: ($) =>
+      seq("import", field("variable", $.environment_variable)),
+    preprocessor_include: ($) => seq("include", $.filename_usage),
+    preprocessor_if: ($) =>
+      seq("if", field("condition", $.preprocessor_expression)),
+    preprocessor_else: (_) => "else",
+    preprocessor_endif: (_) => "endif",
+    preprocessor_once: (_) => "once",
+    preprocessor_expression: ($) =>
+      choice(
+        $.literal,
+        $.variable,
+        alias($.preprocessor_not, $.not),
+        alias($.preprocessor_and, $.and),
+        alias($.preprocessor_or, $.or),
+        $.exists,
+        $.file_exists,
+      ),
+    preprocessor_or: ($) =>
+      prec.left(
+        precedence.or,
+        seq(
+          field("left", $.preprocessor_expression),
+          "||",
+          field("right", $.preprocessor_expression),
+        ),
+      ),
+    preprocessor_and: ($) =>
+      prec.left(
+        precedence.and,
+        seq(
+          field("left", $.preprocessor_expression),
+          "&&",
+          field("right", $.preprocessor_expression),
+        ),
+      ),
+    preprocessor_not: ($) =>
+      prec.right(precedence.not, seq("!", $.preprocessor_expression)),
 
-		environment_variable: $ => $.identifier,
-		variable: $ => $.identifier,
-		function: $ => $.identifier,
-		usage: $ => seq(choice('.', '^'), choice($.variable, $.string)),
+    environment_variable: ($) => $.identifier,
+    variable: ($) => $.identifier,
+    function: ($) => $.identifier,
+    usage: ($) => seq(choice(".", "^"), choice($.variable, $.string)),
 
-		array: $ => seq('{', repeat($.statement), '}'),
-		struct: $ => seq('[', repeat($.statement), ']'),
-		arguments: $ => seq('(', repeat($.statement), ')'),
-		parenthesis: $ => seq('(', $.statement, ')'),
+    array: ($) => seq("{", repeat($.statement), "}"),
+    struct: ($) => seq("[", repeat($.statement), "]"),
+    arguments: ($) => seq("(", repeat($.statement), ")"),
+    parenthesis: ($) => seq("(", $.statement, ")"),
 
-		function_call: $ => prec.right(precedence.function, seq(
-			field('name', $.function),
-			field('arguments', optional($.arguments)),
-			field('body', optional($.array)),
-		)),
+    function_call: ($) =>
+      prec.right(
+        precedence.function,
+        seq(
+          field("name", $.function),
+          field("arguments", optional($.arguments)),
+          field("body", optional($.array)),
+        ),
+      ),
 
-		function_definition: $ => prec.right(precedence.function, seq(
-			'function',
-			field('name', $.identifier),
-			field('arguments', optional($.arguments)),
-			field('body', optional($.array)),
-		)),
+    function_definition: ($) =>
+      prec.right(
+        precedence.function,
+        seq(
+          "function",
+          field("name", $.identifier),
+          field("arguments", optional($.arguments)),
+          field("body", optional($.array)),
+        ),
+      ),
 
-		statement: $ => choice(
-			$.compound,
-			$.expression,
-		),
+    statement: ($) => choice($.compound, $.expression),
 
-		expression: $ => choice(
-			$.literal,
-			$.usage,
-			$.array,
-			$.struct,
-			$.parenthesis,
-			$.not,
-			$.and,
-			$.or,
-			$.in,
-			$.compare,
-			$.not_in,
-			$.function_call,
-			$.function_definition,
-			$.exists,
-			$.file_exists,
-		),
+    expression: ($) =>
+      choice(
+        $.literal,
+        $.usage,
+        $.array,
+        $.struct,
+        $.parenthesis,
+        $.not,
+        $.and,
+        $.or,
+        $.in,
+        $.compare,
+        $.not_in,
+        $.function_call,
+        $.function_definition,
+        $.exists,
+        $.file_exists,
+      ),
 
-		compound: $ => seq(
-			$.usage,
-			choice($.assign, $.concat, $.subtract),
-			repeat(choice($.concat, $.subtract)),
-		),
+    compound: ($) =>
+      seq(
+        $.usage,
+        choice($.assign, $.concat, $.subtract),
+        repeat(choice($.concat, $.subtract)),
+      ),
 
-		assign: $ => seq('=', $.expression),
-		concat: $ => seq('+', $.expression),
-		subtract: $ => seq('-', $.expression),
+    assign: ($) => seq("=", $.expression),
+    concat: ($) => seq("+", $.expression),
+    subtract: ($) => seq("-", $.expression),
 
-		not: $ => prec.right(precedence.not, seq(choice('not', '!'), $.expression)),
+    not: ($) =>
+      prec.right(precedence.not, seq(choice("not", "!"), $.expression)),
 
-		...[
-			['and', choice('&&', 'and'), precedence.and],
-			['or', choice('||', 'or'), precedence.or],
-			// ['add', '+', precedence.add],
-			// ['sub', '-', precedence.sub],
-			['compare', choice('==', '!=', '>', '<', '>=', '<='), precedence.add],
-		].reduce((result, [name, operator, precedence]) => {
-			result[name] = $ => prec.left(precedence, seq(
-				field('left', $.expression),
-				field('operator', operator),
-				field('right', $.expression),
-			))
-			return result
-		}, {}),
+    ...[
+      ["and", choice("&&", "and"), precedence.and],
+      ["or", choice("||", "or"), precedence.or],
+      // ['add', '+', precedence.add],
+      // ['sub', '-', precedence.sub],
+      ["compare", choice("==", "!=", ">", "<", ">=", "<="), precedence.add],
+    ].reduce((result, [name, operator, precedence]) => {
+      result[name] = ($) =>
+        prec.left(
+          precedence,
+          seq(
+            field("left", $.expression),
+            field("operator", operator),
+            field("right", $.expression),
+          ),
+        );
+      return result;
+    }, {}),
 
-		...[
-			['in', 'in', precedence.in],
-			['not_in', alias(seq('not', 'in'), 'not in'), precedence.not_in],
-			// ['assign', '=', precedence.assign],
-		].reduce((result, [name, operator, precedence]) => {
-			result[name] = $ => prec.right(precedence, seq(
-				field('left', $.expression),
-				field('operator', operator),
-				field('right', $.expression),
-			))
-			return result
-		}, {}),
+    ...[
+      ["in", "in", precedence.in],
+      ["not_in", alias(seq("not", "in"), "not in"), precedence.not_in],
+      // ['assign', '=', precedence.assign],
+    ].reduce((result, [name, operator, precedence]) => {
+      result[name] = ($) =>
+        prec.right(
+          precedence,
+          seq(
+            field("left", $.expression),
+            field("operator", operator),
+            field("right", $.expression),
+          ),
+        );
+      return result;
+    }, {}),
 
-		literal: $ => choice(
-			$.string,
-			$.number,
-			$.boolean,
-		),
+    literal: ($) => choice($.string, $.number, $.boolean),
 
-		number: $ => $.decimal,
-		boolean: _ => choice('true', 'false'),
-		exists: $ => seq('exists', '(', $.environment_variable, ')'),
-		file_exists: $ => seq('file_exists', '(', $.filename_usage, ')'),
-
-	},
-})
+    number: ($) => $.decimal,
+    boolean: (_) => choice("true", "false"),
+    exists: ($) => seq("exists", "(", $.environment_variable, ")"),
+    file_exists: ($) => seq("file_exists", "(", $.filename_usage, ")"),
+  },
+});
